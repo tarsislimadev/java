e14bb7623847f5203309278e71ee9af86f868dd2
Tarsis <br.tmvdl@gmail.com>
2023-01-01 17:40:01 -0300

    Fixing Main.java file

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,23 +1,59 @@
+import java.util.List;
+
 import java.net.Socket;
 import java.net.ServerSocket;
+
+import java.io.IOException;
+
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.InputStream;
+
+class Request {
+  public String firstLine = "";
+
+  public Request(InputStream inputStream) throws IOException {
+    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+    BufferedReader reader = new BufferedReader(inputStreamReader);
+
+    String s = "";
+
+    this.firstLine = reader.readLine();
+
+    while ((s = reader.readLine()) != "") {
+      System.out.println("header: " + s);
+    }
+
+    while ((s = reader.readLine()) != "") {
+      System.out.println("body: " + s);
+    }
+
+  }
+
+}
 
 class Response {
-  private String getFirstLine() {
-    return "HTTP/1.1 OK 200";
+  InputStream inputStream = null;
+
+  public Response(InputStream inputStream) {
+    this.inputStream = inputStream;
   }
 
-  private String[] getHeaderLines() {
-    return [];
+  public String getFirstLine() {
+    return "HTTP/1.1 200 OK";
   }
 
-  private String getBodyString() {
+  public String getContentType() {
+    return "Content-Type: text/html";
+  }
+
+  public String getBodyString() {
     return "";
   }
-
 }
 
 class Main {
@@ -26,19 +62,34 @@ class Main {
     ServerSocket ss = new ServerSocket(80);
     Socket s = ss.accept();
 
-    InputStreamReader in = new InputStreamReader(s.getInputStream());
-    BufferedReader bf = new BufferedReader(in);
-    System.out.println(bf.readLine());
+    InputStream inputStream = s.getInputStream();
+    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+    BufferedReader reader = new BufferedReader(inputStreamReader);
 
-    Response response = new Response()
+    OutputStream outputStream = s.getOutputStream();
+    OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream);
+    BufferedWriter writer = new BufferedWriter(outputStreamWriter);
 
-    PrintWriter pr = new PrintWriter(s.getOutputStream());
-    pr.println(response.getFirstLine());
+    String string = "";
 
-    pr.println("");
+    while ((string = reader.readLine()) != null) {
+      System.out.println(string);
+    }
 
-    pr.println(response.getBodyString());
+    // Response response = new Response(inputStream);
 
-    pr.flush();
+    // writer.write(response.getFirstLine());
+    // writer.newLine();
+    
+    // writer.write(response.getContentType());
+    // writer.newLine();
+    
+    // writer.write("");
+    // writer.newLine();
+    
+    // writer.write(response.getBodyString());
+    // writer.newLine();
+
+    // writer.flush();
   }
 }
